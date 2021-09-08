e08600e801ddd83aca469836e5386c792318e05d
Jhonathan Araujo <jhonathanaraujobr@hotmail.com>
2021-09-08 13:14:41 -0300

    Teste verbo HTTP PATCH

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PatchTest.Data;
+using PatchTest.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PatchTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly MovieContext _context;
+
+        public MoviesController(MovieContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
+        {
+            return await _context.Movie.ToListAsync();
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<ActionResult<IEnumerable<Movie>>> PartiallyUpdateMovie(int id, [FromBody] JsonPatchDocument<Movie> movieModified)
+        {
+            if (movieModified == null)
+                return BadRequest();
+
+            var movie = await _context.Movie.FirstOrDefaultAsync(m => m.Id == id);
+            if (movie is null)
+                return NotFound();
+
+            movieModified.ApplyTo(movie, ModelState);
+
+            var isValid = TryValidateModel(movie);
+            if (!isValid)
+                return BadRequest(ModelState);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Data/MovieContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PatchTest.Model;
+
+namespace PatchTest.Data
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
